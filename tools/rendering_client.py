Several small changes: fixed light in second image, normalizing images to [-0.75, 0.75] instead of [-1,1], some new functions in tf.py, removed some hard-coded paths in infinite_renderer, made configuration get function work correctly if the key is not in the json file (but haven't tested yet)

tools/rendering_client.py
@@ -52,7 +52,11 @@ def main(argv=None):
         im1 = rend.renderView([rad1, el1, az1], light_pos1)
         #scipy.misc.toimage(im1[0], cmin=0, cmax=255).save("output1.png")
 
-        light_pos2 = light_pos1
+        # fixed lighting in the second image, roughly symmetric to the first one which is (-5,-5,-5)
+        light_pos2 = [10., 35, 45]
+        
+        # same lighting in the first and the second image
+        #light_pos2 = light_pos1
         rad2 = random.random() * 0.6 + 1.7
         el2 = int(random.random() * 50 - 10)
         az2 = int(random.random() * 360)

utils/data.py
@@ -32,16 +32,16 @@ class SyncDataReceiver():
         RAD_SCALE = 10. # to make radius in smaller range befor feeding to the net
         image1 = np.fromstring(message[0:49152], dtype='uint8')
         image1 = image1.reshape(128, 128, 3)
-        image1 = (image1 / 255.0 - 0.5) * 2
+        image1 = (image1 / 255.0 - 0.5) * 1.5
 
         image2 = np.fromstring(message[49152:98304], dtype='uint8')
         image2 = image2.reshape(128, 128, 3)
-        image2 = (image2 / 255.0 - 0.5) * 2
+        image2 = (image2 / 255.0 - 0.5) * 1.5
 
         depth2 = np.fromstring(message[98304:131072], dtype='uint16')
         #depth2 = depth2.reshape(128, 128, 1)
         #scipy.misc.toimage(depth2, cmin=0, cmax=65535).save("depth2.png")
-        depth2 = (depth2 / 65535.0 - 0.5) * 2
+        depth2 = (depth2 / 65535.0 - 0.5) * 1.5
 
         tmp_label = np.fromstring(message[131072:131072 + 12*4], dtype='int32')
         rad1 = tmp_label[0] / 100. / RAD_SCALE